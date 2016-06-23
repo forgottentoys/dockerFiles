47af58f2f56a13ec5c3efcde86d186c451907710
Evgenii Poliakov <evgenii.poliakov@gmail.com>
2016-06-23 21:56:48 +0300

    Build script for oel7_jdk1.8 changed

--- gksm/oel7_jdk1.8/build.sh
+++ gksm/oel7_jdk1.8/build.sh
@@ -1,8 +1,9 @@
+IMAGE=oel7_jdk1.8
 if [ ! -z "$1" ]; then
-	echo "docker build --tag=\"gksm/oel7_jdk1.8:${1}\" --file=\"./Dockerfile\" ."
-	docker build --tag="gksm/oel7_jdk1.8:${1}" --file="./Dockerfile" .
-	echo "docker tag -f  gksm/oel7_jdk1.8:${1} gksm/oel7_jdk1.8:latest"
-	docker tag -f gksm/oel7_jdk1.8:${1} gksm/oel7_jdk1.8:latest
+	echo "docker build --tag=\"gksm/$IMAGE:${1}\" --file=\"./Dockerfile\" ."
+	docker build --tag="gksm/$IMAGE:${1}" --file="./Dockerfile" .
+	echo "docker tag -f  gksm/$IMAGE:${1} gksm/$IMAGE:latest"
+	docker tag -f gksm/$IMAGE:${1} gksm/$IMAGE:latest
 else
 	echo "buil version should be set."
 	echo "usage example $0 v2" 
